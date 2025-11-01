ecb1ad8ed78b9365e076c072b6ff1e0cdc550b35
tiltrikt <vladoss339@gmail.com>
2025-11-01 15:43:52 +0100

    feat: simple page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,62 @@
-import { useLaunchParams, useSignal, miniApp } from '@tma.js/sdk-react';
-import { AppRoot } from '@telegram-apps/telegram-ui';
+import {useState, useCallback} from 'react';
+import {useLaunchParams, useSignal, miniApp} from '@tma.js/sdk-react';
+import {AppRoot, Input, List, Divider} from '@telegram-apps/telegram-ui';
 import {MainButton} from "@/components/MainButton.ts";
-
 
 export function App() {
   const lp = useLaunchParams();
   const isDark = useSignal(miniApp.isDark);
+  const [query, setQuery] = useState('');
+  const [minPrice, setMinPrice] = useState('');
+  const [maxPrice, setMaxPrice] = useState('');
+
+  const handleSetMinPrice = (e) => {
+    const value = e.target.value;
+    const onlyNumbers = value.replace(/\D/g, "");
+    setMinPrice(onlyNumbers);
+  };
+
+  const handleSetMaxPrice = (e) => {
+    const value = e.target.value;
+    const onlyNumbers = value.replace(/\D/g, "");
+    setMaxPrice(onlyNumbers);
+  };
+
+  const handleClick = useCallback(() => {
+    console.log(minPrice, minPrice, query);
+  }, []);
 
   return (
-    <AppRoot
-      appearance={isDark ? 'dark' : 'light'}
-      platform={['macos', 'ios'].includes(lp.tgWebAppPlatform) ? 'ios' : 'base'}
-    >
-      <MainButton>
-      </MainButton>
-    </AppRoot>
-  );
+  <AppRoot
+    appearance={isDark ? 'dark' : 'light'}
+    platform={['macos', 'ios'].includes(lp.tgWebAppPlatform) ? 'ios' : 'base'}
+  >
+    <List>
+      {/*<Section>*/}
+        <Input
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+          placeholder="Search query"
+        />
+        <Divider/>
+        <Input
+          value={minPrice}
+          onChange={e => handleSetMinPrice(e)}
+          placeholder="Min price"
+          inputMode="numeric"
+        />
+        <Divider/>
+        <Input
+          value={maxPrice}
+          onChange={e => handleSetMaxPrice(e)}
+          placeholder="Max price"
+          inputMode="numeric"
+        />
+      {/*</Section>*/}
+    </List>
+
+    <MainButton onClick={handleClick}/>
+  </AppRoot>
+)
+  ;
 }
--- src/components/MainButton.ts
+++ src/components/MainButton.ts
@@ -1,18 +1,19 @@
-import { useEffect } from 'react';
-import { mainButton } from '@tma.js/sdk-react';
+import {useEffect} from 'react';
+import {mainButton} from '@tma.js/sdk-react';
 
-/**
- * Component which controls the Back Button visibility.
- */
-export function MainButton() {
+export function MainButton({onClick}: { onClick: () => void }) {
 
   useEffect(() => {
     mainButton.show();
-    mainButton.enable();
     return () => {
       mainButton.hide();
     };
   }, []);
+
+  useEffect(() => {
+    mainButton.show();
+    mainButton.onClick(onClick);
+  }, [onClick]);
 
   return null;
 }
