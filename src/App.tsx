0eab2c93032ef072082ca143b8f6212842ded593
tiltrikt <vladoss339@gmail.com>
2025-11-01 15:51:50 +0100

    fix: remove divide

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {useState, useCallback} from 'react';
 import {useLaunchParams, useSignal, miniApp} from '@tma.js/sdk-react';
-import {AppRoot, Input, List, Divider} from '@telegram-apps/telegram-ui';
+import {AppRoot, Input, List} from '@telegram-apps/telegram-ui';
 import {MainButton} from "@/components/MainButton.ts";
 
 export function App() {
@@ -32,27 +32,23 @@ export function App() {
     platform={['macos', 'ios'].includes(lp.tgWebAppPlatform) ? 'ios' : 'base'}
   >
     <List>
-      {/*<Section>*/}
         <Input
           value={query}
           onChange={e => setQuery(e.target.value)}
           placeholder="Search query"
         />
-        <Divider/>
         <Input
           value={minPrice}
           onChange={e => handleSetMinPrice(e)}
           placeholder="Min price"
           inputMode="numeric"
         />
-        <Divider/>
         <Input
           value={maxPrice}
           onChange={e => handleSetMaxPrice(e)}
           placeholder="Max price"
           inputMode="numeric"
         />
-      {/*</Section>*/}
     </List>
 
     <MainButton onClick={handleClick}/>
